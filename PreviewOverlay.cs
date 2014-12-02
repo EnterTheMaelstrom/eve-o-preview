4bcbea467ba622c828ed6ee2b7fed972ae2ec51b
MakariAeron <philip_246@hotmail.com>
2014-12-02 12:21:10 -0600

    Initial Commit

---
+++ PreviewOverlay.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PreviewToy
+{
+    public partial class PreviewOverlay : Form
+    {
+        private Preview parent;
+        private Color original_color;
+        public PreviewOverlay(Preview parent)
+        {
+            this.parent = parent;
+            InitializeComponent();
+            original_color = overlay_area.BackColor;
+        }
+
+        private void PreviewOverlay_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, MouseEventArgs e)
+        {
+            this.parent.render_area_Click(sender, e);
+        }
+
+        private void client_label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PreviewToy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("?")]
+[assembly: AssemblyProduct("PreviewToy")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("04f08f8d-9e98-423b-acdb-4effb31c0d35")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.14.0.0")]
